697ac900cf483c2bd3b866c5242e9267482384ab
James McGaa <jemcgaa@meta.com>
2024-12-12 15:02:23 -0500

    2019 p14 cleaned up and optimzied

--- Advent2019/src/p14.kt
+++ Advent2019/src/p14.kt
@@ -1,7 +1,7 @@
+@file:Suppress("LocalVariableName")
+
 import java.io.File
 import kotlin.math.ceil
-import kotlin.math.min
-import kotlin.math.roundToInt
 
 class Formula(
     val output: Pair<String, Long>,
@@ -9,8 +9,10 @@ class Formula(
 )
 
 fun main() {
+    val FUEL = "FUEL"
+    val TARGET = 1000000000000L
+
     val formulas = mutableMapOf<String, Formula>()
-    val oreFormulas = mutableMapOf<String, Formula>()
     File("inputs/input14.txt").forEachLine {
         val inputStr = it.split("=>")[0].trim()
         val outputStr = it.split("=>")[1].trim()
@@ -22,64 +24,47 @@ fun main() {
             val inputChem = pair.trim().split(" ")[1]
             Pair(inputChem, inputAmount)
         }
-        if (inputs.any { it.first == "ORE" }) {
-            oreFormulas[output.first] = Formula(output, inputs)
-        }
         formulas[output.first] = Formula(output, inputs)
     }
-    val chemWallet = mutableMapOf("FUEL" to 1L)
-    val partA = minOres(0, chemWallet, formulas, oreFormulas)
-    println(partA)
 
-    val TARGET = 1000000000000L
+    fun innerMinOres(fuelCount: Long): Long{
+        return minOres(mutableMapOf(FUEL to fuelCount), formulas)
+    }
+
+    val partA = innerMinOres(1L)
+    println("Part A: $partA ores minimum")
+
     var low = 1L
     var pointer = low
     var amount = partA
-    while (amount < 1000000000000L) {
+    while (amount < TARGET) {
         pointer *= 2
-        amount = minOres(0, mutableMapOf("FUEL" to pointer), formulas, oreFormulas)
+        amount = innerMinOres(pointer)
     }
     var high = pointer
     while (high > low + 1) {
         val midpoint = (high + low) / 2
-        if (minOres(0, mutableMapOf("FUEL" to midpoint), formulas, oreFormulas) < TARGET) {
+        if (innerMinOres(midpoint) < TARGET) {
             low = midpoint
         } else {
             high = midpoint
         }
     }
-    println("$low, $high")
-    println(minOres(0, mutableMapOf("FUEL" to low), formulas, oreFormulas))
-    println(minOres(0, mutableMapOf("FUEL" to high), formulas, oreFormulas))
+    println("Part B: low $low (answer) fuel to ${innerMinOres(low)} ores VS high $high fuel to ${innerMinOres(low)} ores")
 }
 
-val seen = mutableSetOf<String>()
-val INFINITY = Long.MAX_VALUE
-var BEST_SO_FAR = INFINITY
-
 fun minOres(
-    initalOreCount: Long,
     chemWallet: MutableMap<String, Long>,
     formulas: Map<String, Formula>,
-    oreFormulas: Map<String, Formula>
 ): Long {
-    // Neither optimization is necessary
-//    val strRep = chemWallet.toList().sortedBy { it.first }.toString()
-//    if (strRep in seen || initalOreCount > BEST_SO_FAR) {
-//        return INFINITY
-//    } else {
-//        seen.add(strRep)
-//    }
-
-
-    var oreCount = initalOreCount
+    var oreCount = 0L
     val toBeChecked = chemWallet.keys.toMutableSet()
     while (toBeChecked.isNotEmpty()) {
         val chem = toBeChecked.first()
         val amount = chemWallet[chem]!!
         toBeChecked.remove(chem)
 
-        if (amount < 0) { // Surplus instead of a need to fill
+        if (amount <= 0L) { // "Surplus" instead of a "need" to fill
             continue
         }
 
@@ -89,9 +74,9 @@ fun minOres(
         } else {
             val formula = formulas[chem]!!
             val amountRequiredForReaction = formula.output.second
-            val numReactions = ceil((amount / amountRequiredForReaction.toDouble())).toLong() // Theres exactly 1 way to get each output
-            // Thus we round
-//            if (numReactions == 0L) continue // Unneeded now
+            // There is exactly 1 way to get each output (when inspecting the input), thus we round up
+            val numReactions = ceil((amount / amountRequiredForReaction.toDouble())).toLong()
+
             chemWallet[chem] = chemWallet[chem]!! - numReactions * amountRequiredForReaction
             if (chemWallet[chem] == 0L) {
                 chemWallet.remove(chem)
@@ -105,33 +90,4 @@ fun minOres(
         }
     }
     return oreCount
-
-//    if (chemWallet.values.any { it > 0 }) { // This no longer occurs due to rounding up
-//        val recurs = mutableListOf<Long>()
-//        val valuesToTry = chemWallet.keys.filter { chemWallet[it]!! > 0 } intersect oreFormulas.keys
-//        valuesToTry.forEach { chemToBeStimulus ->
-//            val formula = formulas[chemToBeStimulus]!!
-//            val outputAmount = formula.output.second
-//            recurs.add(
-//                minOres(
-//                    oreCount,
-//                    chemWallet.toMutableMap().apply {
-//                        formulas[chemToBeStimulus]!!.inputs.forEach { inputChem ->
-//                            set(inputChem.first, getOrDefault(inputChem.first, 0) + inputChem.second)
-//                        }
-//                        set(chemToBeStimulus, get(chemToBeStimulus)!! - outputAmount) },
-//                    formulas,
-//                    oreFormulas
-//                )
-//            )
-//
-//        }
-//        val ans =  recurs.minOrNull() ?: INFINITY
-//        BEST_SO_FAR = min(ans, BEST_SO_FAR)
-//        return ans
-//    } else {
-//        val ans =  oreCount
-//        BEST_SO_FAR = min(ans, BEST_SO_FAR)
-//        return ans
-//    }
 }
